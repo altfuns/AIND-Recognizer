4d553fe0733f9cf21fe9838446fb8ca5af86e2ed
Alfonso Aguilar <laltfuns@gmail.com>
2017-07-13 11:05:34 -0600

    Implement the CV, BIC and DIC model selectors

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -75,9 +75,24 @@ class SelectorBIC(ModelSelector):
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        best_num_components = None
+        min_bic_score = float("inf")
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                new_model = self.base_model(n)
+                X, lengths = self.hwords[self.this_word]
+                logL = new_model.score(X, lengths)
+                #p = n^2 + 2*d*n - 1
+                #  BIC = −2 log L + p log N
+                p = n**2 + 2 * len(X[1]) * n - 1
+                bic_score = -2 * logL + p * math.log(len(X))
+                if bic_score < min_bic_score:
+                    min_bic_score = bic_score
+                    best_num_components = n
+            except:
+                if self.verbose:
+                    print("failure on {} with {} states".format(self.this_word, n))
+        return self.base_model(best_num_components)
 
 
 class SelectorDIC(ModelSelector):
@@ -90,19 +105,75 @@ class SelectorDIC(ModelSelector):
     '''
 
     def select(self):
+        """ select best model based on Discriminative Information Criterion (DIC)
+            DIC is distante between likelihood of the word and the average likelihood of the other words
+        :return: GaussianHMM object
+        """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # M number of categories or classe (words)
+        M = len(self.words)
+        best_num_components = self.n_constant #sets the default value to return when all the models fail
+        max_dic_score = float("-inf")
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                new_model = self.base_model(n)
+                X, lengths = self.hwords[self.this_word]
+                # Compute the P(X(i)) formula part. The Likelihood of the word
+                logL = new_model.score(X, lengths)
+                antiL = 0
+                for word in self.words:
+                    if self.this_word != word:
+                        X, lengths = self.hwords[word]
+                        try:
+                            # SUM the Likelihoods of the other words
+                            antiL = antiL + new_model.score(X, lengths)
+                        except:
+                            if self.verbose:
+                                print("failure score on {} with {} states".format(word, n))
+                # Compute the diference between the word Likelihood and the average Likelihood of the other words
+                dic_score = logL - (1/(M-1) * antiL)
+                if dic_score > max_dic_score:
+                    max_dic_score = dic_score
+                    best_num_components = n
+            except:
+                if self.verbose:
+                    print("failure on {} with {} states".format(self.this_word, n))
+        return self.base_model(best_num_components)
 
 
 class SelectorCV(ModelSelector):
-    ''' select best model based on average log Likelihood of cross-validation folds
-
-    '''
+    """ select best model based on average log Likelihood of cross-validation folds
+    :return: GaussianHMM object
+    """
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        # Return a model with the default number of component
+        # when the word hasn't enough number of samples
+        if min(len(self.sequences), 3) < 3:
+            return self.base_model(self.n_constant)
+        split_method = KFold()
+        best_num_components = None
+        best_avg_logL = float("-inf")
+        for num_components in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                logLs = []
+                for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                    # Set the mode with the train Xs
+                    self.X, self.lengths = combine_sequences(cv_train_idx, self.sequences)
+                    new_model = self.base_model(num_components)
+                    X, lengths = combine_sequences(cv_test_idx, self.sequences)
+                    # Compute the Likelihood evaluating the test samples
+                    logLs.append(new_model.score(X, lengths))
+                # Compute the mean of the log Likelihood
+                avg_logL = np.mean(logLs)
+                # Maximize the average Likelihood for the num_components
+                if avg_logL > best_avg_logL:
+                    best_avg_logL = avg_logL
+                    best_num_components = num_components
+            except:
+                if self.verbose:
+                    print("failure on {} with {} states".format(self.this_word, num_components))
+        # Restore the X,lengths inital values
+        self.X, self.lengths = self.hwords[self.this_word]
+        return self.base_model(best_num_components)
